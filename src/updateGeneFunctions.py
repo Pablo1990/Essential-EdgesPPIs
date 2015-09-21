976e9262428ee460d3cd368081fe2f83c10aa333
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-21 10:53:36 +0100

    done updateGeneFunctions algorithm

--- src/updateGeneFunctions.py
+++ src/updateGeneFunctions.py
@@ -1,7 +1,9 @@
 #Code by Pablo Vicente-Munuera
 
 def extractCategoryFrom(line):
-	
+	fields = line.split('\t')
+	categories = fields[3].split('.')
+	return categories[0]
 
 data = open('../data/p3_p13838_Sac_cerev.txt', 'r')
 
@@ -10,8 +12,8 @@ numGenes = sum(1 for line in open('../data/genes.csv', 'r'))
 genesFile = open('../data/genes.csv', 'r') #Arguments
 genesFile.readline() #header
 
-outputFile = open('../data/genes_function.txt', 'w') #The new file
-outputFile.write("GENE;gene func.")
+outputFile = open('../data/genes_function.csv', 'w') #The new file
+outputFile.write("GENE;gene function\n")
 
 count = 0
 
@@ -30,22 +32,34 @@ for gene in genesFile:
 		#print (line)
 		if line.startswith(gene[:-2]): #Shitty characters like '\n' and things like that
 			#print (line)
-			newLine = gene[:2] + ";"
-			newLine = newLine + extractCategoryFrom(line)
+			newLine = gene[:-2] + ";"
+			#newLine = newLine +  + ", "
+			categories = []
+			cat = extractCategoryFrom(line)
+			if (cat != '-'):
+				categories.append(int(cat))
+			else:
+				break
 			#outputFile.write(line)
 			for xline in data:
 				if xline.startswith('X\t'):
-					outputFile.write(xline)
+					cat = extractCategoryFrom(xline)
+					if (cat.startswith('X') == False):
+						#newLine = newLine + cat + ", "
+						categories.append(int(cat))
 				else:
 					break
 			found = True
+			outputFile.write(newLine + str(list(set(categories))))
 			break
 
 	if found == False:
 		notFoundGenes = notFoundGenes + gene[:-2]  + ", "
 		numberOfNotFound = numberOfNotFound + 1
+		outputFile.write(gene[:-2] + "; xxx")
 
 	found = False
+	outputFile.write("\n")
 
 genesFile.close()
 data.close()
