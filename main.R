2ec02d77b46f60244ef5a1a1915ce3981bc728ba
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-09-24 11:17:49 +0100

    ranking the communicabilityAngle

--- main.R
+++ main.R
@@ -4,10 +4,30 @@ source('~/Documents/Dropbox/MScBioinformatics/Thesis/Project/Essential-EdgesPPIs
 library('igraph')
 
 #Examples:
-adjacencyM <- matrix( c(0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1), nrow=4, ncol = 4, byrow = T)
+#adjacencyM <- matrix( c(0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1), nrow=4, ncol = 4, byrow = T)
 
-communicabilityAngle(adjacencyM)
+#communicabilityAngle(adjacencyM)
 
-communicabilityDistance(adjacencyM)
+#communicabilityDistance(adjacencyM)
 
-edge.betweenness(graph.adjacency(adjacencyM, mode = "undirected"))
+# ------- Results
+
+adjacencyData <- read.csv(header = F, file = "data/YeastS-main.txt", sep = "\t")
+
+adjacencyMatrix <- as.matrix(adjacencyData)
+
+diag(adjacencyMatrix) <- 0
+
+resultsEdgeBetw <- edge.betweenness(graph.adjacency(adjacencyMatrix, mode = "undirected"))
+
+resultsEdgeBetw[order(resultsEdgeBetw)]
+
+resultsComAngle <- communicabilityAngle(adjacencyMatrix)
+
+resultsComDist <- communicabilityDistance(adjacencyMatrix)
+
+#write.table (resultsEdgeBetw, file = "data/resultsEdgeBetweennessPPIsYeast.txt", row.names = F, col.names = F)
+
+resultsComDist[upper.tri(resultsComDist, diag = FALSE)] <- 0
+resultsComDist[adjacencyMatrix == 0] <- 0
+
