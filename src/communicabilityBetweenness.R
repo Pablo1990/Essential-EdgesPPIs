e27aec13c202b9a85169f6d51a5b25df8922c90f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-10-05 14:30:53 +0100

    new file

---
+++ src/communicabilityBetweenness.R
@@ -0,0 +1,6 @@
+#Developed by Pablo Vicente-Munuera
+#Translate to R from:
+#http://networkx.github.io/documentation/latest/_modules/networkx/algorithms/centrality/communicability_alg.html#communicability_betweenness_centrality
+communicabilityBetweennessCentrality <- function(adjacencyM){
+  
+}
