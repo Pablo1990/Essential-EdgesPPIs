e2590f57ef9d8b7ec38d0a3b397bd416912c0fa0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-10-05 15:00:21 +0100

    start using, and debugging

---
+++ src/usingCommunicabilityBetweenness.R
@@ -0,0 +1,17 @@
+#Developed by Pablo Vicente-Munuera
+source('~/Documents/Dropbox/MScBioinformatics/Thesis/Project/Essential-EdgesPPIs/src/communicabilityBetweenness.R', echo=TRUE)
+
+adjacencyData <- read.csv(header = F, file = "data/YeastS-main.txt", sep = "\t")
+
+vertices <- read.csv2(header = F, file = "docs/proteins.csv")
+
+vertices <- vertices$V2
+
+row.names(adjacencyData) <- vertices
+colnames(adjacencyData) <- vertices
+
+adjacencyMatrix <- as.matrix(adjacencyData)
+
+diag(adjacencyMatrix) <- 0
+
+cbc <- communicabilityBetweennessCentrality(adjacencyMatrix)
