33cad91222c61377fcdd7b9ae068169788a69c00
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-10-20 14:42:14 +0100

    sum of adjacent betweenness

--- src/AdjacentBetweenness.py
+++ src/AdjacentBetweenness.py
@@ -10,23 +10,22 @@ for ppi in orderPPIs:
 	protein = ppi.split(';')
 
 	vertices.seek(0)
+	totalBetweenness = 0
 	protein1 = False
-	betwProtein1 = 0
 	protein2 = False
-	betwProtein2 = 0
 	for betweenness in vertices:
 		betw = betweenness.split(';')
 		#print protein
 		#print betw
 		if protein[1] == betw[0]:
 			protein1 = True
-			betwProtein1 = float(betw[1][:-1])
+			totalBetweenness = totalBetweenness + float(betw[1][:-1])
 		elif protein[2][:-2] == betw[0]:
 			protein2 = True
-			betwProtein2 = float(betw[1][:-1])
+			totalBetweenness = totalBetweenness + float(betw[1][:-1])
 
 		if protein1 and protein2:
-			finalCebc.write(ppi[:-2] + ';' + str(min(betwProtein1, betwProtein2)) + '\n')
+			finalCebc.write(ppi[:-2] + ';' + str(totalBetweenness) + '\n')
 			break
 
 	#break
