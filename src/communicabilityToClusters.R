0bc8fb4394d0d59b9769e100e52f313d8a17f925
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-10-22 11:15:09 +0100

    all files the communicability

--- src/communicabilityToClusters.R
+++ src/communicabilityToClusters.R
@@ -3,15 +3,21 @@
 library('igraph')
 source('~/Documents/Dropbox/MScBioinformatics/Thesis/Project/Essential-EdgesPPIs/src/lib/translationMatlabCode.R', echo=TRUE)
 
-clusterPPI1 <- read.csv2(header = F, file = "data/cluster/EBCEssentialPPIs1.csv")
-edges <- clusterPPI1[,1:2]
-
-graphCluster <- graph.edgelist(as.matrix(edges), directed = FALSE)
-
-adjacencyCluster <- as.matrix(get.adjacency(graphCluster))
-
-cACluster <- communicabilityAngle(adjacencyCluster)
-
-cDCluster <- communicabilityDistance(adjacencyCluster)
-
-cECluster <- communicabilityEdges(adjacencyCluster)
+for (i in 1:126) {
+  clusterPPI1 <- read.csv2(header = F, file = paste0("data/cluster/EBCEssentialPPIs", i, ".csv"))
+  edges <- clusterPPI1[,1:2]
+  
+  graphCluster <- graph.edgelist(as.matrix(edges), directed = FALSE)
+  
+  adjacencyCluster <- as.matrix(get.adjacency(graphCluster))
+  
+  cACluster <- communicabilityAngle(adjacencyCluster)
+  
+  cDCluster <- communicabilityDistance(adjacencyCluster)
+  
+  cECluster <- communicabilityEdges(adjacencyCluster)
+  
+  clusterPPI1 <- cbind(clusterPPI1, cACluster[as.matrix(edges)], cDCluster[as.matrix(edges)], cECluster[as.matrix(edges)])
+  
+  write.csv2(clusterPPI1, header = F, file = paste0("data/cluster/EBCEssentialCommPPIs",i, ".csv"))
+}
