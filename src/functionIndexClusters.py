32ce9abc20f9a81d38f9cf727ca5a80a2074c4e4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-10-28 11:59:03 +0000

    more info for the functionIndex

--- src/functionIndexClusters.py
+++ src/functionIndexClusters.py
@@ -3,15 +3,19 @@ import glob, os
 
 genesFile = open('../data/genes_function_Essentials.csv', 'r')
 
+clusterInfo = open('../data/clustersTogether/EBCEssentialPPIsSpinglassWeightedFunctionIndex.csv', 'w')
+clusterInfo.write('file;Function Index;Essential proteins;Mean EBC\n')
+
 
 os.chdir("/Users/pablovm1990/Documents/Dropbox/MScBioinformatics/Thesis/Project/Essential-EdgesPPIs/data/clustersPPIs")
-for file in glob.glob("EBCEssentialPPIs[0-9]*.csv"):
+for file in glob.glob("EBCEssentialPPIsSpinglassWeighted[0-9]*.csv"):
 	ppis = open(file, 'r')
 	ppis.readline()
 
 	totalGenes = 0
 	totalFunctions = 0
 	totalEssentials = 0
+	totalEBC = 0
 	genes = {}
 	for ppi in ppis:
 		ppiFields = ppi.split(';')
@@ -24,22 +28,26 @@ for file in glob.glob("EBCEssentialPPIs[0-9]*.csv"):
 		for geneRow in genesFile:
 			gene = geneRow.split(';')
 			#print ppiFields
-			if gene[0] == ppiFields[0]:
+			if gene[0] == ppiFields[0] and not(ppiFields[1] in genes):
 				totalGenes = totalGenes + 1
 				function = len(gene[2][:-1].split(', '))
 				genes[ppiFields[0]] = function
 				totalFunctions = totalFunctions + function
 				totalEssentials = totalEssentials + (1 if gene[1] == '1' else 0)
-			elif gene[0] == ppiFields[1]:
+				totalEBC = totalEBC + float(ppiFields[3][:-1].replace(',', '.'))
+			elif gene[0] == ppiFields[1] and not(ppiFields[1] in genes):
 				function = len(gene[2][:-1].split(', '))
 				genes[ppiFields[1]] = function
 				totalFunctions = totalFunctions + function
 				totalEssentials = totalEssentials + (1 if gene[1] == '1' else 0)
 				totalGenes = totalGenes + 1
 				#print functionGene2
+				totalEBC = totalEBC + float(ppiFields[3][:-1].replace(',', '.'))
 
 			if ppiFields[0] in genes and ppiFields[1] in genes:
 				break
 
-	print genes
-	print (float(totalFunctions) / float(totalGenes))
+	#print file
+	#print "Function Index: " + str((float(totalFunctions) / float(totalGenes)))
+	#print "Essential proteins: " + str(totalEssentials)
+	clusterInfo.write(file + ";" + str((float(totalFunctions) / float(totalGenes))).replace('.', ',') + ";" + str(float(totalEssentials) / float(totalGenes)).replace('.', ',') + ";" + str(totalEBC).replace('.', ',') + "\n")
