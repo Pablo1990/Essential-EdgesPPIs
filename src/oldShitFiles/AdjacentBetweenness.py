d02b8f15d41d0a352ce36b6dc53322ca2c3b8e1e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-11-03 15:23:24 +0000

    useless files

---
+++ src/oldShitFiles/AdjacentBetweenness.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#Developed by Pablo Vicente-Munuera
+
+orderPPIs = open('../data/orderPPIsFromBetweennessCentrality.csv', 'r')
+vertices = open('../data/verticesBetweenness.csv', 'r')
+vertices.readline()
+
+finalCebc = open('../data/adjacentBetweennessPPIs.csv', 'w')
+
+for ppi in orderPPIs:
+	protein = ppi.split(';')
+
+	vertices.seek(0)
+	totalBetweenness = 0
+	protein1 = False
+	protein2 = False
+	for betweenness in vertices:
+		betw = betweenness.split(';')
+		#print protein
+		#print betw
+		if protein[1] == betw[0]:
+			protein1 = True
+			totalBetweenness = totalBetweenness + float(betw[1][:-1])
+		elif protein[2][:-2] == betw[0]:
+			protein2 = True
+			totalBetweenness = totalBetweenness + float(betw[1][:-1])
+
+		if protein1 and protein2:
+			finalCebc.write(ppi[:-2] + ';' + str(totalBetweenness) + '\n')
+			break
+
+	#break
+
+
+finalCebc.close()
