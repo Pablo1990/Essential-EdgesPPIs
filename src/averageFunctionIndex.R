1375c15d64e9109beb65611398d9497692a6d420
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-11-05 16:02:15 +0000

    done

--- src/averageFunctionIndex.R
+++ src/averageFunctionIndex.R
@@ -2,14 +2,14 @@
 
 library('igraph')
 
-ppis <- read.csv2(file = "data/PPIsInfo/PPIsFunctionIndexEssentialityStop.csv")
+ppis <- read.csv2(file = "data/PPIsInfo/PPIsFunctionIndexEstradaEssential.csv")
 
 #ppis$index <- as.numeric(levels(ppis$index)[ppis$index])
 
-i <- 4957
-
-mean(ppis[1:i,][ppis[1:i,]$Type.of.link=="E",]$index)
+i <- 6609
 
 mean(ppis[1:i,][ppis[1:i,]$Type.of.link=="EE",]$index)
+
+mean(ppis[1:i,][ppis[1:i,]$Type.of.link=="E",]$index)
 
 mean(ppis[1:i,][ppis[1:i,]$Type.of.link=="NE",]$index)
